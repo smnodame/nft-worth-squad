85267a63f603ffbbc342bbb6031beba6c6dd3a56
Kollan House <kollanh@gmail.com>
2021-09-09 15:36:26 -0400

    feat: adds link with smart chain with example fetch, adds todos for future data, adds libs for constants and abis

---
+++ capture-worker/lib/bunnies.js
@@ -0,0 +1,109 @@
+const bunnies = {
+    "Swapsies": {
+        "id": 0,
+        "total_count": 112,
+        "total_burned": 63,
+    },
+    "Drizzle": {
+        "id": 1,
+        "total_count": 101,
+        "total_burned": 16,
+    },
+    "Blueberries": {
+        "id": 2,
+        "total_count": 74,
+        "total_burned": 6,
+    },
+    "Circular": {
+        "id": 3,
+        "total_count": 73,
+        "total_burned": 4,
+    },
+    "Sparkle": {
+        "id": 4,
+        "total_count": 130,
+        "total_burned": 21,
+    },
+    "Sleepy": {
+        "id": 5,
+        "total_count": 98555,
+        "total_burned": 0,
+    },
+    "Dollop": {
+        "id": 6,
+        "total_count": 33076,
+        "total_burned": 0,
+    },
+    "Twinkle": {
+        "id": 7,
+        "total_count": 55300,
+        "total_burned": 0,
+    },
+    "Churro": {
+        "id": 8,
+        "total_count": 54205,
+        "total_burned": 0,
+    },
+    "Sunny": {
+        "id": 9,
+        "total_count": 68608,
+        "total_burned": 0,
+    },
+    "Hiccup": {
+        "id": 10,
+        "total_count": 1865,
+        "total_burned": 0,
+    },
+    "Bullish": {
+        "id": 11,
+        "total_count": 8626,
+        "total_burned": 0,
+    },
+    "Stormy Easter ‘21": {
+        "id": 12,
+        "total_count": 11105,
+        "total_burned": 0,
+    },
+    "Flipsie Easter ‘21": {
+        "id": 13,
+        "total_count": 11350,
+        "total_burned": 0,
+    },
+    "Cakeston Easter ‘21": {
+        "id": 14,
+        "total_count": 16857,
+        "total_burned": 0,
+    },
+    "Easter '21 Champions": {
+        "id": 15,
+        "total_count": 499,
+        "total_burned": 0,
+    },
+    "Syrup Soak": {
+        "id": 16,
+        "total_count": 13898,
+        "total_burned": 0,
+    },
+    "Claire": {
+        "id": 17,
+        "total_count": 7288,
+        "total_burned": 0,
+    },
+    "Lottie": {
+        "id": 18,
+        "total_count": 17466,
+        "total_burned": 0,
+    },
+    "Lucky": {
+        "id": 19,
+        "total_count": 9320,
+        "total_burned": 0,
+    },
+    "Baller": {
+        "id": 20,
+        "total_count": 42,
+        "total_burned": 0,
+    },
+}
+
+export { bunnies }
---
+++ capture-worker/lib/constants.js
@@ -0,0 +1,7 @@
+const PANCAKE_BUNNY_CONTRACT_ADDRESS = "0xDf7952B35f24aCF7fC0487D01c8d5690a60DBa07"
+
+const API_URL = "https://api.treasureland.market/v2/v1"
+
+const WEB_URL = "https://www.treasureland.market/assets"
+
+export { PANCAKE_BUNNY_CONTRACT_ADDRESS, API_URL, WEB_URL }
